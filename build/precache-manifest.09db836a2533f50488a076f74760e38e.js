619d21a8f948cb2791d753973a1486b07c78f736
Gee Vee <ilovesciencefb@gmail.com>
2019-02-11 22:40:29 -0800

    built files

---
+++ build/precache-manifest.09db836a2533f50488a076f74760e38e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "11e5f1e0ab4cc8f8a282",
+    "url": "/static/js/main.11e5f1e0.chunk.js"
+  },
+  {
+    "revision": "23d4d7eb13b8a9d27857",
+    "url": "/static/js/2.23d4d7eb.chunk.js"
+  },
+  {
+    "revision": "11e5f1e0ab4cc8f8a282",
+    "url": "/static/css/main.f0fd7c8e.chunk.css"
+  },
+  {
+    "revision": "19eb0d40a64d6d51e33dc809b5ff71b8",
+    "url": "/index.html"
+  }
+];
