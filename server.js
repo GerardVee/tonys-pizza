cf123850a5db5490593a93d0e7feca09aafafd93
Gee Vee <ilovesciencefb@gmail.com>
2019-02-12 22:32:32 -0800

    a restart

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) =>
+app.get('/*', (_, res) =>
 {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
