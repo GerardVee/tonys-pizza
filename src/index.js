acfe5da28392827046bf8c5f7493dc2b8ac0acbd
Gerardo Valenzuela <gjvalenz@usc.edu>
2023-01-04 07:56:41 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactRouter from './router/router';
-import { BrowserRouter as Router }  from 'react-router-dom';
+import { HashRouter as Router }  from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
